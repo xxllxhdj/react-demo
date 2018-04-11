6584ea14330fc8edad409ad2ba6eec32c6b4c0bb
xxllxhdj <tx082xuxl@126.com>
2018-04-11 15:28:04 +0800

    计算衍生数据

--- src/containers/visibleTodoList.js
+++ src/containers/visibleTodoList.js
@@ -2,20 +2,22 @@ import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
 import TodoList from '../components/todoList';
 
-const getVisibleTodos = (todos, filter) => {
-    switch (filter) {
-        case 'SHOW_COMPLETED':
-            return todos.filter(t => t.completed);
-        case 'SHOW_ACTIVE':
-            return todos.filter(t => !t.completed);
-        case 'SHOW_ALL':
-        default:
-            return todos;
-    }
-};
+import { getVisibleTodos } from '../selectors/todo';
+
+// const getVisibleTodos = (todos, filter) => {
+//     switch (filter) {
+//         case 'SHOW_COMPLETED':
+//             return todos.filter(t => t.completed);
+//         case 'SHOW_ACTIVE':
+//             return todos.filter(t => !t.completed);
+//         case 'SHOW_ALL':
+//         default:
+//             return todos;
+//     }
+// };
 
 const mapStateToProps = state => ({
-    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    todos: getVisibleTodos(state)
 });
 
 const mapDispatchToProps = dispatch => ({
---
+++ src/selectors/todo.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+const getVisibilityFilter = state => state.visibilityFilter;
+const getTodos = state => state.todos;
+
+export const getVisibleTodos = createSelector(
+    [getVisibilityFilter, getTodos],
+    (visibilityFilter, todos) => {
+        switch (visibilityFilter) {
+            case 'SHOW_COMPLETED':
+                return todos.filter(t => t.completed);
+            case 'SHOW_ACTIVE':
+                return todos.filter(t => !t.completed);
+            case 'SHOW_ALL':
+            default:
+                return todos;
+        }
+    }
+);
